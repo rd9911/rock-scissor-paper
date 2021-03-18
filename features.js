f62f139841fb35616198a498d5fb34a4b2c3b85d
Avazjon Yusufjonov <azizaziz9911@mail.ru>
2021-03-18 20:04:14 +0900

    Change local score vars to global vars. Add conditional to find the winner in main()

--- features.js
+++ features.js
@@ -1,21 +1,24 @@
+// to track scores after each round
+    let playerScore = 0;
+    let computerScore = 0;
+
+// computer will choose one of ['rock', 'scissors', 'paper']
 function computerPlay() {
     let choices = ['rock', 'scissors', 'paper'];
     let index = Math.floor(Math.random() * choices.length);
     return choices[index]
 }
 
+// player enters his choice.
 function playerPlay() {
     let playerSelection = prompt('So what are you gonna choose: [Rock], [Scissors', 'Paper').toLowerCase()
     return playerSelection;
 }
 
-
+// function that compares computer and player's choice and determine the winner of the round.
 function game(playerSelection, computerSelection) {
-    let computerSelection = computerPlay();
-    let playerScore = 0;
-    let computerScore = 0;
 
-    if (playerSelection !== null) {
+    if ((playerSelection !== null) && (['rock', 'scissors', 'paper'].includes(playerSelection))) {
         if (playerSelection === computerSelection) {
             console.log('Draw');
         } else if ((playerSelection === 'rock') && (computerSelection === 'paper')) {
@@ -40,10 +43,22 @@ function game(playerSelection, computerSelection) {
     } else {
         console.log('Please refresh the site and enter your choice again!');
     }
+
 }
 
+// main function that will run the code for 5 times and determine the winner.
 function main() {
     for (let i=1; i<6; i++) {
         game(playerPlay, computerSelection);
     }
+
+    if (playerScore > computerScore) {
+        console.log('You are a winner! Congratulations!!!');
+      } else if (playerScore < computerScore) {
+        console.log('Computer is a winner! Computers are power!!!');
+      } else if (playerScore === computerScore) {
+        console.log('Draw. Try again!');
+      }
 }
+
+main()
